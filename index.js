77e1258612ed535b86c8ce1096e5175d57681962
Wend Carlos <wendcarlos18@hotmail.com>
2021-09-28 19:48:17 -0300

    assembling orders

--- index.js
+++ index.js
@@ -3,8 +3,14 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/users', (request, response) => {
-    return response.send('hello world')
+const customerRequest = []
+
+app.post('/customerRequest', (request, response) => {
+    const {id, order, clientName, price, status} = request.body
+
+    const order = {id, order, clientName, price, status}
+
+    return response.json(customerRequest)
 })
 
 app.listen(port, () => {
