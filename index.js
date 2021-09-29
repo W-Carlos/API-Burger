48a7954a87cba3f49fcde67c47f157309d4315e7
Wend Carlos <wendcarlos18@hotmail.com>
2021-09-29 16:35:41 -0300

    created post and get routes, installed uuid library

--- index.js
+++ index.js
@@ -1,15 +1,26 @@
 const express = require('express')
+const uuid = require('uuid')
 
 const app = express()
 const port = 3000
+app.use(express.json())
 
 const customerRequest = []
 
+// Rota de criar pedidos
 app.post('/customerRequest', (request, response) => {
-    const {id, order, clientName, price, status} = request.body
 
-    const order = {id, order, clientName, price, status}
+    const {order, clienteName, price} = request.body
 
+    const newOrder = {id:uuid.v4(), order, clienteName, price}
+
+    customerRequest.push(newOrder)
+
+    return response.status(201).json(newOrder)
+})
+
+// Rota que mostra os pedidos
+app.get('/customerRequest', (request, response) => {
     return response.json(customerRequest)
 })
 
