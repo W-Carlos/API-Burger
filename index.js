93549fa6e0eb24e6f73c676e1060e615513869f0
Wend Carlos <wendcarlos18@hotmail.com>
2021-10-01 16:14:30 -0300

    middleware verify id

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { request, response } = require('express')
 const express = require('express')
 const uuid = require('uuid')
 
@@ -6,6 +7,21 @@ const port = 3000
 app.use(express.json())
 
 const firstOrder = []
+
+const checkId = (request, response, next) => {
+    const {id} = request.params
+
+    const index = firstOrder.findIndex(order => order.id === id)
+
+    if(index < 0) {
+        return response.status(404).json({message: "Order not found"})
+    }
+
+    request.orderIndex = index
+    request.orderId = id
+
+    next()
+}
 
 // Rota de criar pedidos
 app.post('/firstOrder', (request, response) => {
@@ -25,19 +41,14 @@ app.get('/firstOrder', (request, response) => {
 })
 
 // Rota que altera pedido ja feito
-app.put('/firstOrder/:id', (request, response) => {
+app.put('/firstOrder/:id', checkId, (request, response) => {
 
-    const {id} = request.params
+    const index = request.orderIndex
+    const id = request.orderId
 
     const {order, clienteName, price} = request.body
 
-    const updateOrder = {id, order, clienteName, price}
-
-    const index = firstOrder.findIndex(order => order.id === id)
-
-    if(index < 0) {
-        return response.status(404).json({message: "Order not found"})
-    }
+    const updateOrder = {id, order, clienteName, price, status: "Em preparação"}
 
     firstOrder[index] = updateOrder
 
@@ -45,14 +56,8 @@ app.put('/firstOrder/:id', (request, response) => {
 })
 
 // Rota que deleta pedido
-app.delete('/firstOrder/:id', (request, response) => {
-    const {id} = request.params
-
-    const index = firstOrder.findIndex(order => order.id === id)
-
-    if(index < 0) {
-        return response.status(404).json({message: "Order not found"})
-    }
+app.delete('/firstOrder/:id', checkId, (request, response) => {
+    const index = request.orderIndex
 
     firstOrder.splice(index, 1)
 
@@ -60,15 +65,9 @@ app.delete('/firstOrder/:id', (request, response) => {
 })
 
 // Rota que retorna um pedido específico pelo id
-app.get('/firstOrder/:id', (request, response) => {
+app.get('/firstOrder/:id', checkId, (request, response) => {
 
-    const {id} = request.params
-
-    const index = firstOrder.findIndex(order => order.id === id)
-
-    if(index < 0) {
-        return response.status(404).json({message: "Order not found"})
-    }
+    const index = request.orderIndex
 
     orderId = firstOrder[index]
 
@@ -76,23 +75,18 @@ app.get('/firstOrder/:id', (request, response) => {
 })
 
 // Rota que altera o status do pedido recebido pelo id para "Pronto".
-app.patch('/firstOrder/:id', (request, response) => {
-    const {id} = request.params
+app.patch('/firstOrder/:id', checkId, (request, response) => {
     const {order, clienteName, price, status} = request.body
 
+    const index = request.orderIndex
+    const id = request.orderId
+
     const changeStatus = {id, order, clienteName, price, status}
-
-    const index = firstOrder.findIndex(order => order.id === id)
-
-    if(index < 0) {
-        return response.status(404).json({message: "Order not found"})
-    }
 
     firstOrder[index] = changeStatus
 
     return response.json(changeStatus)
 })
-
 
 app.listen(port, () => {
     console.log(`🚀Server started on port ${port}`)
